de17c64f782a719355e33cf484dcb90173d3520d
Александр Шандыба <s.shandyba@mail.ru>
2021-03-15 02:47:30 +0500

    Добавлено окно авторизации

--- RepairAirplanesWPF/Classes/RepairAirplanesDataManager.cs
+++ RepairAirplanesWPF/Classes/RepairAirplanesDataManager.cs
@@ -2,17 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using DBManager;
 namespace RepairAirplanesWPF.Classes
 {
     class RepairAirplanesDataManager
     {
-        private object Context;
+        private DBLoader<Thread> AirplaneDataLoader;
+        private DBLoader<Thread> PersonDataLoader;
+        private DBLoader<Thread> RepairStatusDataLoader;
+        private DBLoader<Thread> RepairWorkDataLoader;
+        private DBLoader<Thread> RepairPartDataLoader;
+        private DBLoader<Thread> RequiredRepairPartDataLoader;
+        private DBLoader<Thread> RequiredRepairWorkDataLoader;
+        private DBLoader<Thread> PermissionDataLoader;
+        private DBLoader<Thread> PermissionGroupDataLoader;
+        private DBLoader<Thread> PermissionForGroupDataLoader;
+        private DBLoader<Thread> AuthorizationDataLoader;
 
-        public RepairAirplanesDataManager(object context)
+        public RepairAirplanesDataManager()
         {
-            this.Context = context;
+
         }
 
     }
